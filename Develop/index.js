5735c31a7d87a76880fc83c632b948602dec3008
Andre M <andre.ji.moseley@gmail.com>
2021-04-25 20:57:35 -0400

    Complete generateMarkdown file structure and prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const genMarkdown = require('./utils/generateMarkdown'); 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput){
+                return true;}
+              else{
+                console.log('Please enter your project title!');
+                return false;}
+            }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project. This can include: What was your motivation? Why did you build this project? What problem does it solve? What did you learn? What makes this project stand out? (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true;}
+              else{
+                console.log('Please enter a description for your project!');
+                return false;}
+            }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput){
+                return true;}
+              else{
+                console.log('Please enter the installation steps!');
+                return false;}
+            }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provides instructions on how to utilize this application. (Required)',
+        validate: installationInput => {
+            if (installationInput){
+                return true;}
+              else{
+                console.log('Please enter the usage instructions!');
+                return false;}
+            }
+    },
+    {
+        type: 'input',
+        name: 'collaborators',
+        message: 'List collaborators, tutorials or thrid-party assests utilized in this project',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select the licenses for your project.',
+        choices: ['Apache v2', 'GNU GPLv3', 'MIT', 'ISC', 'GNU GPLv2']
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Select the licenses for your project.',
+        choices: ['Apache v2', 'GNU GPLv3', 'MIT', 'ISC', 'GNU GPLv2']
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'Enter your Git profile. (Required)',
+        validate: gitInput => {
+            if (gitInput){
+                return true;}
+              else{
+                console.log('Please enter your Git profile!');
+                return false;}
+            }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Do you have an email address?',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve,reject) => {
+        fs.writeFile('../Develop/README.md', data, err => {
+            if (err)
+            { reject(err);
+              return; }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
-init();
+init()
+.then(generateMarkdown => {
+    return writeToFile()
+});
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,78 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license)
+    { return '';}
+
+    return ` badge `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license)
+    { return '';}
+
+    return `${license}`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (!license)
+    { return '';}
+
+    return `# License`;
+}
+
+// TODO: Create a function that returns the email address to the README
+// If there is no email, return an empty string
+function emailEntered(email) {
+  if (!email)
+  { return '';}
+
+  return `Email address: ${email}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  # Table of Contents
+  * [Description](# Description)
+  * [Installation](# Installation)
+  * [Usage](# Usage)
+  * [Contributing](# Contributing)
+  * [License](# License)
+  * [Tests](# Tests)
+  * [Questions](# Questions?)
+
+  # Description
+     ${data.description}
+
+  # Installation 
+     ${data.installation}
+
+  # Usage 
+     ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ${renderLicenseBadge(data.license)} ${renderLicenseLink(data.license)}
+
+  # Contributing
+     ${data.contributing}
+
+  # Tests
+     ${data.tests}
+
+  # Questions?
+     ${data.contact}
+     ${emailEntered(data.email)}
+
+  # References
+     ${data.references}
 `;
 }
 
